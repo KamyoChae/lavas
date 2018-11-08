210c973db174e44e007e6c6f7d2e71b01d476188
kamyochae <1465678596@qq.com>
2018-11-08 14:40:42 +0800

    lavas config

---
+++ first_demo/.lavas/skeleton.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+
+import Skeleton from 'E:\\Workers\\lavas\\first_demo\\core\\Skeleton.vue';
+
+
+export default new Vue({
+    components: {
+        
+        Skeleton,
+        
+    },
+    template: `
+        <div>
+            
+            <skeleton id="skeleton" style="display:none"/>
+            
+        </div>
+    `
+});
